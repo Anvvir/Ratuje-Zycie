462811974715a93f273ca9b7cc0673ed9a7beb15
Emil Pawlak <Emil.Szymon.Pawlak@gmail.com>
2018-10-03 21:55:57 +0200

    expanded decision paths in life in danger mode

---
+++ danger-page/script.js
@@ -0,0 +1,16 @@
+function animalchoice() {
+  document.getElementById("background").src="catordog.jpg";
+  document.getElementById("first-logo").src="dog.png";
+  document.getElementById("second-logo").src="cat.png";
+  document.getElementById("first-text").innerHTML="Pies";
+  document.getElementById("second-text").innerHTML="Kot";
+  document.getElementById("question").innerHTML="Które zwierze potrzebuje twojej pomocy?";
+}
+function humanchoice() {
+  document.getElementById("background").src="adultorchild.jpg";
+  document.getElementById("first-logo").src="adult.png";
+  document.getElementById("second-logo").src="child.png";
+  document.getElementById("first-text").innerHTML="Dorosły";
+  document.getElementById("second-text").innerHTML="Bobas";
+  document.getElementById("question").innerHTML="Tą osobą jest dorosły, czy małe dziecko?";
+}
