641e2c145207a19833e600b16301ba95e77da982
Emil Pawlak <Emil.Szymon.Pawlak@gmail.com>
2018-10-11 23:26:06 +0200

    Added few decision screens, reaserching for valuable and meaningful content for further panels

--- danger-page/script/script.js
+++ danger-page/script/script.js
@@ -10,3 +10,15 @@ function catchoice() {
   document.getElementById("catscreen").style.display = "block";
   document.getElementById("animalscreen").style.display = "none";
 }
+function dogchoice() {
+  document.getElementById("dogscreen").style.display = "block";
+  document.getElementById("animalscreen").style.display = "none";
+}
+function adultchoice() {
+  document.getElementById("adultscreen").style.display = "block";
+  document.getElementById("humanscreen").style.display = "none";
+}
+function childchoice() {
+  document.getElementById("childscreen").style.display = "block";
+  document.getElementById("humanscreen").style.display = "none";
+}
