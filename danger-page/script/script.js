d11760d855ec50097d7c09be5240b5280a93c8ce
Emil Pawlak <Emil.Szymon.Pawlak@gmail.com>
2018-10-15 20:05:20 +0200

    Finished whole cat path! (no RWD yet)

--- danger-page/script/script.js
+++ danger-page/script/script.js
@@ -1,24 +1,119 @@
-function animalchoice() {
-  document.getElementById("animalscreen").style.display = "block";
-  document.getElementById("mainscreen").style.display = "none";
+function homechoice() {
+  document.getElementById("main_screen").style.display = "block";
 }
 function humanchoice() {
-  document.getElementById("humanscreen").style.display = "block";
-  document.getElementById("mainscreen").style.display = "none";
+  document.getElementById("human_screen").style.display = "block";
+  document.getElementById("main_screen").style.display = "none";
 }
-function catchoice() {
-  document.getElementById("catscreen").style.display = "block";
-  document.getElementById("animalscreen").style.display = "none";
+  function adultchoice() {
+    document.getElementById("adult_screen").style.display = "block";
+    document.getElementById("human_screen").style.display = "none";
+  }
+  function childchoice() {
+    document.getElementById("child_screen").style.display = "block";
+    document.getElementById("human_screen").style.display = "none";
+  }
+function animalchoice() {
+  document.getElementById("animal_screen").style.display = "block";
+  document.getElementById("main_screen").style.display = "none";
 }
-function dogchoice() {
-  document.getElementById("dogscreen").style.display = "block";
-  document.getElementById("animalscreen").style.display = "none";
-}
-function adultchoice() {
-  document.getElementById("adultscreen").style.display = "block";
-  document.getElementById("humanscreen").style.display = "none";
-}
-function childchoice() {
-  document.getElementById("childscreen").style.display = "block";
-  document.getElementById("humanscreen").style.display = "none";
-}
+  function dogchoice() {
+    document.getElementById("dog_screen").style.display = "block";
+    document.getElementById("animal_screen").style.display = "none";
+  }
+  function catchoice() {
+    document.getElementById("cat_screen").style.display = "block";
+    document.getElementById("animal_screen").style.display = "none";
+  }
+    function main_unconcious_cat_choice() {
+      document.getElementById("cat_screen").style.display = "none";
+      document.getElementById("main_cat_unconcious_screen").style.display = "block";
+    }
+      function cat_unconcious_first_choice() {
+        document.getElementById("main_cat_unconcious_screen").style.display = "none";
+        document.getElementById("cat_unconcious_first_screen").style.display = "block";
+      }
+      function cat_unconcious_second_choice() {
+        document.getElementById("main_cat_unconcious_screen").style.display = "none";
+        document.getElementById("cat_unconcious_second_screen").style.display = "block";
+      }
+    function main_burn_cat_choice() {
+      document.getElementById("cat_screen").style.display = "none";
+      document.getElementById("main_cat_burn_screen").style.display = "block";
+    }
+      function cat_burn_first_choice() {
+        document.getElementById("main_cat_burn_screen").style.display = "none";
+        document.getElementById("cat_burn_first_screen").style.display = "block";
+      }
+      function cat_burn_second_choice() {
+        document.getElementById("main_cat_burn_screen").style.display = "none";
+        document.getElementById("cat_burn_second_screen").style.display = "block";
+      }
+      function cat_burn_third_choice() {
+        document.getElementById("cat_burn_second_screen").style.display = "none";
+        document.getElementById("cat_burn_third_screen").style.display = "block";
+      }
+      function cat_burn_forth_choice() {
+        document.getElementById("cat_burn_second_screen").style.display = "none";
+        document.getElementById("cat_burn_forth_screen").style.display = "block";
+      }
+    function main_bleeding_cat_choice() {
+      document.getElementById("cat_screen").style.display = "none";
+      document.getElementById("main_cat_bleeding_screen").style.display = "block";
+    }
+      function cat_bleeding_first_choice() {
+        document.getElementById("main_cat_bleeding_screen").style.display = "none";
+        document.getElementById("cat_bleeding_first_screen").style.display = "block";
+      }
+      function cat_bleeding_second_choice() {
+        document.getElementById("main_cat_bleeding_screen").style.display = "none";
+        document.getElementById("cat_bleeding_second_screen").style.display = "block";
+      }
+    function main_fracture_cat_choice() {
+      document.getElementById("cat_screen").style.display = "none";
+      document.getElementById("main_cat_fracture_screen").style.display = "block";
+    }
+      function cat_fracture_first_choice() {
+        document.getElementById("main_cat_fracture_screen").style.display = "none";
+        document.getElementById("cat_fracture_first_screen").style.display = "block";
+      }
+      function cat_fracture_second_choice() {
+        document.getElementById("main_cat_fracture_screen").style.display = "none";
+        document.getElementById("cat_fracture_second_screen").style.display = "block";
+      }
+    function main_foreign_cat_choice() {
+      document.getElementById("cat_screen").style.display = "none";
+      document.getElementById("main_cat_foreign_screen").style.display = "block";
+    }
+      function main_cat_foreign_throat_choice() {
+        document.getElementById("main_cat_foreign_screen").style.display = "none";
+        document.getElementById("main_cat_foreign_throat_screen").style.display = "block";
+      }
+        function cat_foreign_throat_first_choice() {
+          document.getElementById("main_cat_foreign_throat_screen").style.display = "none";
+          document.getElementById("cat_foreign_throat_first_screen").style.display = "block";
+        }
+        function cat_foreign_throat_second_choice() {
+          document.getElementById("main_cat_foreign_throat_screen").style.display = "none";
+          document.getElementById("cat_foreign_throat_second_screen").style.display = "block";
+        }
+          function cat_foreign_throat_first_problem_choice() {
+            document.getElementById("cat_foreign_throat_second_screen").style.display = "none";
+            document.getElementById("cat_foreign_throat_first_problem_screen").style.display = "block";
+          }
+          function cat_foreign_throat_second_problem_choice() {
+            document.getElementById("cat_foreign_throat_second_screen").style.display = "none";
+            document.getElementById("cat_foreign_throat_second_problem_screen").style.display = "block";
+          }
+      function main_cat_foreign_nose_choice() {
+        document.getElementById("main_cat_foreign_screen").style.display = "none";
+        document.getElementById("main_cat_foreign_nose_screen").style.display = "block";
+      }
+      function main_cat_foreign_eye_choice() {
+        document.getElementById("main_cat_foreign_screen").style.display = "none";
+        document.getElementById("main_cat_foreign_eye_screen").style.display = "block";
+      }
+      function main_cat_foreign_mouth_choice() {
+        document.getElementById("main_cat_foreign_screen").style.display = "none";
+        document.getElementById("main_cat_foreign_mouth_screen").style.display = "block";
+      }
