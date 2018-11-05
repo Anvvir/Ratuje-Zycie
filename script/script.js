c60068b4cd59a2d549982f579efe672537d28876
Emil Pawlak <Emil.Szymon.Pawlak@gmail.com>
2018-11-05 18:48:56 +0100

    troubleshooting github pages error 404

---
+++ script/script.js
@@ -0,0 +1,6 @@
+function firstdiscriptionfunction() {
+  document.getElementById("firstdiscription").style.display="block";
+}
+function seconddiscriptionfunction() {
+  document.getElementById("seconddiscription").style.display="block";
+}
