455c42ce37fe9e36041aa8d81478db00fed199da
Emil Pawlak <Emil.Szymon.Pawlak@gmail.com>
2018-11-05 19:20:05 +0100

    Changed <a> tags to link correctly again

--- home-page/script/script.js
+++
@@ -1,6 +0,0 @@
-function firstdiscriptionfunction() {
-  document.getElementById("firstdiscription").style.display="block";
-}
-function seconddiscriptionfunction() {
-  document.getElementById("seconddiscription").style.display="block";
-}
